302bac3075f88440b2326337a19b999486d353a9
Shea McAuley <s179049@students.aie.edu.au>
2018-04-10 16:17:14 -0700

    Baked

---
+++ Assets/_Scripts/PlayerShoot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour {
+
+    private const string PLAYER_TAG = "Player";
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera Referenced!");
+            this.enabled = false;
+        }
+
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+
+        }
+    }
+
+    [Client]
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range, mask))
+        {
+            if (hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShot(hit.collider.name);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShot(string _ID)
+    {
+        Debug.Log(_ID + " has been shot.");
+
+        
+
+        
+    }
+}
+
+
---
+++ Assets/playerSetup.cs
@@ -0,0 +1,78 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Player))]
+public class playerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componetToDisable;
+
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            DisableComponets();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if(sceneCamera != null)
+            {
+                Camera.main.gameObject.SetActive(false);
+            }
+            
+        }
+
+
+        RegisterPlayer();
+
+    }
+
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(_netID, _player);
+    }
+    void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponets()
+    {
+        for (int i = 0; i < componetToDisable.Length; i++)
+        {
+            componetToDisable[i].enabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            Camera.main.gameObject.SetActive(true);
+        }
+
+        GameManager.UnRegisterPlayer(transform.name);
+    } 
+
+
+}
